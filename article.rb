f42048895a7332f7cda063169f9a91ad73032814
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-29 09:42:24 +0300

    intialized the author,article and magazine

---
+++ article.rb
@@ -0,0 +1,14 @@
+class Article
+    attr_reader :magazine, :title, :author
+        @@all = []
+        def initialize(author, magazine, title)
+            @author = author
+            @magazine = magazine
+            @title = title
+            @@all << self
+        end
+
+      def self.all
+        @@all
+      end
+end
--- author.rb
+++ author.rb
@@ -1,7 +1,13 @@
 class Author
     attr_reader :name
-    
+    @@all = []
+
     def initialize(name)
       @name = name
+      @@all << self
     end
+
+    def self.all
+        @@all
+      end
   end
